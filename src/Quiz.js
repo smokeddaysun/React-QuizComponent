ad98182b1a5bab99ce19d2ce23689c9fdd85eb5d
Aaron Kirson <aaronkirson@outlook.com>
2019-05-26 09:18:54 +0100

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,30 +1,20 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
 
-
-
-
 class Quiz extends Component {
-   constructor(props)
-   {
-super(props)
-this.state ={
-    quiz_position:1
+constructor(props){
+  super(props)
+  this.state = {quiz_position: 1}
+}
+
+render(){
+  return(
+<div><div className="QuizQuestion">{quizData.quiz_questions[0]
+  .instruction_text} </div></div>
+)
+  }
 
 }
 
-   }
-
-    render() {
-        return (
-            <div>
-                <div className="QuizQuestion"> {quizData.quiz_questions[0].instruction_text } </div>
-            </div>
-        );
-    }
-}
-
-export default Quiz;
-
-About
+export default Quiz
